81257b31770abf4a722d64cc2d6af84bf4990045
VAlexei <alexnadmirom@gmail.com>
2022-12-25 20:19:57 +1200

    OV2

--- learn.js
+++ learn.js
@@ -2,9 +2,9 @@ console.log("let's rock!");
 
 const years = 1986;
 console.log(years);
+var userName = "Alex121";
 
-{
-  var userName = "Alex1";
-  
+function names() {
+  console.log(userName);
 }
-console.log(userName);
+names();
